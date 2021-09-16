a3a76043a5f9af934180674d2a13eac6170461bb
David Dellsperger <david.dellsperger@gmail.com>
2021-09-16 08:17:46 -0500

    Clean up Pong, Add EndMusicCommandTest

--- src/test/java/net/ddellspe/music/bot/commands/PingCommandTest.java
+++ src/test/java/net/ddellspe/music/bot/commands/PingCommandTest.java
@@ -1,7 +1,6 @@
 package net.ddellspe.music.bot.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -19,12 +18,12 @@ import reactor.test.StepVerifier;
 
 public class PingCommandTest {
   private static Snowflake GUILD_ID = Snowflake.of("123456");
-  private MusicAudioManager manager;
+  private MusicAudioManager mockManager;
 
   @BeforeEach
   public void before() {
-    manager = Mockito.mock(MusicAudioManager.class);
-    MusicAudioManager.set(GUILD_ID, manager);
+    mockManager = Mockito.mock(MusicAudioManager.class);
+    MusicAudioManager.set(GUILD_ID, mockManager);
   }
 
   @Test
@@ -36,19 +35,19 @@ public class PingCommandTest {
   @Test
   public void testGetFilterChannelReturnsProperValue() {
     Snowflake chatChannel = Snowflake.of("111111");
-    when(manager.getChatChannel()).thenReturn(Snowflake.of("111111"));
+    when(mockManager.getChatChannel()).thenReturn(Snowflake.of("111111"));
     PingCommand cmd = new PingCommand();
     assertEquals(chatChannel, cmd.getFilterChannel(GUILD_ID));
   }
 
   @Test
-  public void testHandlerForEvent() {
+  public void testPongResponseIsReturned() {
     MessageCreateEvent mockEvent = Mockito.mock(MessageCreateEvent.class);
     Message mockMessage = Mockito.mock(Message.class);
     MessageChannel mockMessageChannel = Mockito.mock(MessageChannel.class);
-    when(mockEvent.getMessage()).thenReturn(mockMessage);
     Mono<MessageChannel> channel = Mono.just(mockMessageChannel);
-    StepVerifier.create(channel).expectNext(mockMessageChannel).verifyComplete();
+
+    when(mockEvent.getMessage()).thenReturn(mockMessage);
     when(mockMessage.getChannel()).thenReturn(channel);
     when(mockMessageChannel.createMessage("Pong"))
         .thenReturn(Mono.just(Mockito.mock(Message.class)));
@@ -56,6 +55,7 @@ public class PingCommandTest {
     PingCommand cmd = new PingCommand();
     cmd.handle(mockEvent).block();
 
-    verify(mockMessageChannel, times(1)).createMessage(anyString());
+    StepVerifier.create(channel).expectNext(mockMessageChannel).verifyComplete();
+    verify(mockMessageChannel, times(1)).createMessage("Pong");
   }
 }
